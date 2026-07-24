a0fa2cc7da8cd9923484c012024d36b79548775d
alexxcarter988-code <alexxcarter988@gmail.com>
2026-07-24 23:43:02 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,34 @@
+# Basic Python Practice
+# Author: Alex Carter
+
+print("Python Practice")
+
+name = "Alex"
+age = 37
+
+print("Name:", name)
+print("Age:", age)
+
+# Simple calculation
+
+first_number = 10
+second_number = 20
+
+total = first_number + second_number
+
+print("Total:", total)
+
+
+# Working with a list
+
+interests = [
+    "Cybersecurity",
+    "Linux",
+    "Networking",
+    "Photography"
+]
+
+print("\nInterests:")
+
+for interest in interests:
+    print("-", interest)
